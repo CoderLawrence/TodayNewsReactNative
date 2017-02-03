e0faa200748ecf496d75484ae97296b8fe758896
Lawrence <791785648@qq.com>
2017-02-03 16:11:44 +0800

    添加滑动菜单工具类

---
+++ app/src/tools/WJScrollMenuInfo.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lawrence on 2017/2/3.
+ */
+
+import React, { Component } from 'react';
+
+var WJScrollMenuInfo = {
+
+};
+
+module.exports = WJScrollMenuInfo;
---
+++ app/src/tools/WJScrollMenuView.js
@@ -0,0 +1,63 @@
+/**
+ * Created by lawrence on 2017/2/3.
+ */
+
+import React, { Component, PropTypes } from 'react';
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    ScrollView,
+    AppRegistry,
+    TouchableOpacity,
+} from 'react-native';
+
+/**
+ *  定制滑动菜单工具类
+ */
+export default class WJScrollMenuView extends Component {
+
+    static defaultProps = {
+        backgroundColor: '#ffffff',
+    };
+
+    static propTypes = {
+        menuInfos: PropTypes.array,
+        backgroundColor: PropTypes.string,
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <View style={Styles.container}>
+                <ScrollView>
+                    {
+                        this.props.menuInfos.map((title, i)=> {
+                            <TouchableOpacity
+                                key = {i}
+                            >
+                                <View style={Styles.selectedItem}>
+                                    <Text>{title}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        })
+                    }
+                </ScrollView>
+            </View>
+        );
+    }
+}
+
+const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+    },
+
+    selectedItem: {
+    },
+});
