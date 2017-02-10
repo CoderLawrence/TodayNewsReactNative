c95fdaafc91f9e2125a00e2955b9883df7d67861
Lawrence <791785648@qq.com>
2017-02-10 14:55:50 +0800

    个人中心主页面

--- app/src/component/personal/Personal.js
+++ app/src/component/personal/Personal.js
@@ -8,6 +8,8 @@ import {
     Text,
     Image,
     StyleSheet,
+    ListView,
+    TouchableHightlight,
 } from 'react-native';
 
 import Constants from '../../common/Constants';
@@ -18,15 +20,49 @@ export default class Personal extends Component {
 
     constructor(props) {
         super(props);
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows([
+                'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+            ])
+        };
+
+        //bind
+        this._renderRow = this._renderRow.bind(this);
+        this._renderSeparator = this._renderSeparator.bind(this);
     }
 
     render () {
         return (
             <View style={styles.container}>
-                <PersonalHeader name = 'CoderLawrence' clickButton={() => this.clickButton()}/>
-
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this._renderRow}
+                    renderSeparator={this._renderSeparator}
+                    renderHeader={() => <PersonalHeader name = 'CoderLawrence' clickButton={() => this.clickButton()}/>}
+                    renderSectionHeader={()=> <View style={{height: 10, backgroundColor: '#e5e5e5'}}></View>}
+                >
+                </ListView>
             </View>
         )
+    }
+
+    _renderRow(rowData) {
+        return (
+            <View style={{height: 50, justifyContent: 'center'}}>
+                <Text>{rowData}</Text>
+            </View>
+        );
+    }
+
+    _renderSeparator(sectionID: number, rowID: number, adjacentRowHighlighted: bool) {
+        return (
+            <View
+                key = {rowID}
+                style={{height: 1, backgroundColor: '#e5e5e5'}}
+            >
+            </View>
+        );
     }
 
     clickButton() {
