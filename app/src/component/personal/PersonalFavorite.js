c58081ffb771f962bdb8caf320ad454d357917ba
Lawrence <791785648@qq.com>
2017-02-13 20:23:00 +0800

    引入下拉刷新组件

--- app/src/component/personal/PersonalFavorite.js
+++ app/src/component/personal/PersonalFavorite.js
@@ -10,19 +10,27 @@ import {
 } from 'react-native';
 
 import BaseNavigationBar from '../../common/BaseNavigationBar';
-import RefreshListView from '../../tools/RefreshListView';
+import PullRefreshScrollView from 'react-native-pullrefresh-scrollview';
 
 export default class PersonalFavorite extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <BaseNavigationBar title={'收藏历史'} leftItemTitle={'返回'} leftItemFunc={this.props.navigator.pop}/>
-                <RefreshListView
-                    backgroundColor={'red'}
-                    headerView={<View style={{height: 100, backgroundColor: 'red'}}></View>}
-                />
+                <PullRefreshScrollView ref = "PullRefresh" onRefresh = {() => this.onRefresh()}>
+                    <View><Text>Scroll</Text></View>
+                </PullRefreshScrollView>
             </View>
         )
+    }
+
+    onRefresh() {
+
     }
 }
 
