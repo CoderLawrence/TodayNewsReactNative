3ee3dbf6586e7b9aa2e9efc43a33f0153d5f0f62
Lawrence <791785648@qq.com>
2017-06-22 11:04:08 +0800

    修改工程结构

---
+++ app/src/component/activity/Activity.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lawrence on 2017/2/28.
+ */
+
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    Image,
+    ListView,
+    StyleSheet,
+} from 'react-native';
+
+export default class Activity extends  Component {
+
+}
